b23bc18215164c1d617cc1f6985dfa0fe1ff052b
Rayid Ghani <rayidghani@gmail.com>
2017-04-27 09:00:10 -0500

    re-did magicloops

--- magicloops.py
+++ magicloops.py
@@ -17,11 +17,15 @@ import pylab as pl
 import matplotlib.pyplot as plt
 from scipy import optimize
 import time
+import seaborn as sns
 
+# for jupyter notebooks
+#%matplotlib inline
 
-%matplotlib inline
+# if you're running this in a jupyter notebook, print out the graphs
+NOTEBOOK = 0
 
-def define_clfs_params():
+def define_clfs_params(grid_size):
 
     clfs = {'RF': RandomForestClassifier(n_estimators=50, n_jobs=-1),
         'ET': ExtraTreesClassifier(n_estimators=10, n_jobs=-1, criterion='entropy'),
@@ -35,7 +39,7 @@ def define_clfs_params():
         'KNN': KNeighborsClassifier(n_neighbors=3) 
             }
 
-    grid = { 
+    large_grid = { 
     'RF':{'n_estimators': [1,10,100,1000,10000], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
     'LR': { 'penalty': ['l1','l2'], 'C': [0.00001,0.0001,0.001,0.01,0.1,1,10]},
     'SGD': { 'loss': ['hinge','log','perceptron'], 'penalty': ['l2','l1','elasticnet']},
@@ -47,28 +51,53 @@ def define_clfs_params():
     'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1,10],'kernel':['linear']},
     'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],'algorithm': ['auto','ball_tree','kd_tree']}
            }
-    return clfs, grid
+    
+    small_grid = { 
+    'RF':{'n_estimators': [10,100], 'max_depth': [5,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10]},
+    'LR': { 'penalty': ['l1','l2'], 'C': [0.00001,0.001,0.1,1,10]},
+    'SGD': { 'loss': ['hinge','log','perceptron'], 'penalty': ['l2','l1','elasticnet']},
+    'ET': { 'n_estimators': [10,100], 'criterion' : ['gini', 'entropy'] ,'max_depth': [5,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10]},
+    'AB': { 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1,10,100,1000,10000]},
+    'GB': {'n_estimators': [10,100], 'learning_rate' : [0.001,0.1,0.5],'subsample' : [0.1,0.5,1.0], 'max_depth': [5,50]},
+    'NB' : {},
+    'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+    'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1,10],'kernel':['linear']},
+    'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],'algorithm': ['auto','ball_tree','kd_tree']}
+           }
+    
+    test_grid = { 
+    'RF':{'n_estimators': [1], 'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+    'LR': { 'penalty': ['l1'], 'C': [0.01]},
+    'SGD': { 'loss': ['perceptron'], 'penalty': ['l2']},
+    'ET': { 'n_estimators': [1], 'criterion' : ['gini'] ,'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+    'AB': { 'algorithm': ['SAMME'], 'n_estimators': [1]},
+    'GB': {'n_estimators': [1], 'learning_rate' : [0.1],'subsample' : [0.5], 'max_depth': [1]},
+    'NB' : {},
+    'DT': {'criterion': ['gini'], 'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+    'SVM' :{'C' :[0.01],'kernel':['linear']},
+    'KNN' :{'n_neighbors': [5],'weights': ['uniform'],'algorithm': ['auto']}
+           }
+    
+    if (grid_size == 'large'):
+        return clfs, large_grid
+    elif (grid_size == 'small'):
+        return clfs, small_grid
+    elif (grid_size == 'test'):
+        return clfs, test_grid
+    else:
+        return 0, 0
 
-def clf_loop(models_to_run, clfs, grid, X, y):
-    for n in range(1, 2):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-        for index,clf in enumerate([clfs[x] for x in models_to_run]):
-            print models_to_run[index]
-            parameter_values = grid[models_to_run[index]]
-            for p in ParameterGrid(parameter_values):
-                try:
-                    clf.set_params(**p)
-                    print clf
-                    y_pred_probs = clf.fit(X_train, y_train).predict_proba(X_test)[:,1]
-                    #threshold = np.sort(y_pred_probs)[::-1][int(.05*len(y_pred_probs))]
-                    #print threshold
-                    print precision_at_k(y_test,y_pred_probs,.05)
-                    #plot_precision_recall_n(y_test,y_pred_probs,clf)
-                except IndexError, e:
-                    print 'Error:',e
-                    continue
+def generate_binary_at_k(y_scores, k):
+    cutoff_index = int(len(y_scores) * (k / 100.0))
+    test_predictions_binary = [1 if x < cutoff_index else 0 for x in range(len(y_scores))]
+    return test_predictions_binary
 
-
+def precision_at_k(y_true, y_scores, k):
+    preds_at_k = generate_binary_at_k(y_scores, k)
+    #precision, _, _, _ = metrics.precision_recall_fscore_support(y_true, preds_at_k)
+    #precision = precision[1]  # only interested in precision for label 1
+    precision = precision_score(y_true, preds_at_k)
+    return precision
 
 def plot_precision_recall_n(y_true, y_prob, model_name):
     from sklearn.metrics import precision_recall_curve
@@ -83,6 +112,7 @@ def plot_precision_recall_n(y_true, y_prob, model_name):
         pct_above_thresh = num_above_thresh / float(number_scored)
         pct_above_per_thresh.append(pct_above_thresh)
     pct_above_per_thresh = np.array(pct_above_per_thresh)
+    
     plt.clf()
     fig, ax1 = plt.subplots()
     ax1.plot(pct_above_per_thresh, precision_curve, 'b')
@@ -91,32 +121,61 @@ def plot_precision_recall_n(y_true, y_prob, model_name):
     ax2 = ax1.twinx()
     ax2.plot(pct_above_per_thresh, recall_curve, 'r')
     ax2.set_ylabel('recall', color='r')
+    ax1.set_ylim([0,1])
+    ax1.set_ylim([0,1])
+    ax2.set_xlim([0,1])
     
     name = model_name
     plt.title(name)
     #plt.savefig(name)
     plt.show()
-
-def precision_at_k(y_true, y_scores, k):
-    threshold = np.sort(y_scores)[::-1][int(k*len(y_scores))]
-    y_pred = np.asarray([1 if i >= threshold else 0 for i in y_scores])
-    return metrics.precision_score(y_true, y_pred)
+    
 
 
-def main(): 
-    clfs, grid = define_clfs_params()
-    models_to_run=['KNN','RF','LR','ET','AB','GB','NB','DT']
-    #get X and y
+def clf_loop(models_to_run, clfs, grid, X, y):
+    results_df =  pd.DataFrame(columns=('model_type','clf', 'parameters', 'auc-roc','p_at_5', 'p_at_10', 'p_at_20'))
+    for n in range(1, 2):
+        # create training and valdation sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+        for index,clf in enumerate([clfs[x] for x in models_to_run]):
+            print models_to_run[index]
+            parameter_values = grid[models_to_run[index]]
+            for p in ParameterGrid(parameter_values):
+                try:
+                    clf.set_params(**p)
+                    y_pred_probs = clf.fit(X_train, y_train).predict_proba(X_test)[:,1]
+                    # you can also store the model, feature importances, and prediction scores
+                    # we're only storing the metrics for now
+                    y_pred_probs_sorted, y_test_sorted = zip(*sorted(zip(y_pred_probs, y_test), reverse=True))
+                    results_df.loc[len(results_df)] = [models_to_run[index],clf, p,
+                                                       roc_auc_score(y_test, y_pred_probs),
+                                                       precision_at_k(y_test_sorted,y_pred_probs_sorted,5.0),
+                                                       precision_at_k(y_test_sorted,y_pred_probs_sorted,10.0),
+                                                       precision_at_k(y_test_sorted,y_pred_probs_sorted,20.0)]
+                    if NOTEBOOK == 1:
+                        plot_precision_recall_n(y_test,y_pred_probs,clf)
+                except IndexError, e:
+                    print 'Error:',e
+                    continue
+    return results_df
+
+
+
+def main():
+
+    grid_size = 'test'
+    clfs, grid = define_clfs_params(grid_size)
+    models_to_run=['RF','DT','KNN', 'ET', 'AB', 'GB', 'LR', 'NB']
+
+    df = pd.read_csv("/Users/rayid/Projects/uchicago/Teaching/MLPP-2017/Homeworks/Assignment 2/credit-data.csv")
     features  =  ['RevolvingUtilizationOfUnsecuredLines', 'DebtRatio', 'age', 'NumberOfTimes90DaysLate']
     X = df[features]
     y = df.SeriousDlqin2yrs
-    clf_loop(models_to_run, clfs,grid, X,y)
-
+    results_df = clf_loop(models_to_run, clfs,grid, X,y)
+    if NOTEBOOK == 1:
+        results_df
+    results_df.to_csv('results.csv', index=False)
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
